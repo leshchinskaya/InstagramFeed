04906ef34dadf50aebbe4f7eb5130f662bacf045
Leshchinskaya <mariya@leshchinskaya.local>
2018-01-31 15:32:00 +0300

    init Explore

---
+++ InstagramFeed/Controller/ExplorePhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ExplorePhotoCollectionViewCell.swift
+//  InstagramFeed
+//
+//  Created by Marie on 30.01.18.
+//  Copyright © 2018 Mariya. All rights reserved.
+//
+
+import UIKit
+
+class ExplorePhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
